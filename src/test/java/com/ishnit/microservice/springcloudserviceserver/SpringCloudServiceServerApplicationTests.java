fea6795496fef4d4ebcc5c4c8165a48c1134778c
Nirdesh Mohan <NirdeshM@Gmail.com>
2020-09-21 22:10:56 -0400

    initail1

---
+++ src/test/java/com/ishnit/microservice/springcloudserviceserver/SpringCloudServiceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ishnit.microservice.springcloudserviceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudServiceServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
